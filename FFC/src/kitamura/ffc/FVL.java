78c46c10f8e9b2ec048d13027b0f8d2209f4299f
Yasuhiko Kitamura <ykitamura@kwansei.ac.jp>
2016-06-18 00:15:54 +0900

    済の反映

--- FFC/src/kitamura/ffc/FVL.java
+++ FFC/src/kitamura/ffc/FVL.java
@@ -12,6 +12,7 @@ import javax.swing.JTree;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
 public class FVL extends JFrame implements TreeSelectionListener {
@@ -20,6 +21,7 @@ public class FVL extends JFrame implements TreeSelectionListener {
 
 	Database db;
 	DefaultMutableTreeNode root;
+	DefaultTreeModel model;
 	JTree tree;
 	JLabel jl;
 
@@ -35,6 +37,7 @@ public class FVL extends JFrame implements TreeSelectionListener {
 		root = new DefaultMutableTreeNode("Fighters");
 		// DefaultMutableTreeNode swing = new DefaultMutableTreeNode("Swing");
 		tree = new JTree(root);
+		model = (DefaultTreeModel)tree.getModel();
 		tree.addTreeSelectionListener(this);
 
 		// root.add(swing);
@@ -44,13 +47,13 @@ public class FVL extends JFrame implements TreeSelectionListener {
 		scrollPane.setPreferredSize(new Dimension(500, 500));
 
 		JPanel p = new JPanel();
-		//p.setLayout(new BorderLayout());
-		jl = new JLabel("");	
+		// p.setLayout(new BorderLayout());
+		jl = new JLabel("");
 		jl.setPreferredSize(new Dimension(500, 20));
 		p.add(jl, BorderLayout.NORTH);
 		p.add(scrollPane, BorderLayout.CENTER);
 
-		//getContentPane().add(p, BorderLayout.CENTER);
+		// getContentPane().add(p, BorderLayout.CENTER);
 
 		getContentPane().add(p);
 
@@ -73,74 +76,102 @@ public class FVL extends JFrame implements TreeSelectionListener {
 	void addVideo() {
 		ArrayList<Video> v = db.getVideo();
 		for (int i = 0; i < v.size(); i++) {
-			String header="";
-			if(v.get(i).watch==0) header="[未]"; else header = "[済]";
+			String header = "";
+			if (v.get(i).watch == 0)
+				header = "[未]";
+			else
+				header = "[済]";
 			String path = v.get(i).name;
-			if(path.indexOf("\\")<0) continue;
+			if (path.indexOf("\\") < 0)
+				continue;
 			path = path.substring(2);
-			//System.out.println(path.substring(0,path.indexOf("\\")));
-			root.setUserObject(new DefaultMutableTreeNode(path.substring(0,path.indexOf("\\"))));
+			// System.out.println(path.substring(0,path.indexOf("\\")));
+			root.setUserObject(new DefaultMutableTreeNode(path.substring(0, path.indexOf("\\"))));
 			DefaultMutableTreeNode node = root;
-			path = path.substring(path.indexOf("\\")+1);
-			while(path.indexOf("\\")>0){
+			path = path.substring(path.indexOf("\\") + 1);
+			while (path.indexOf("\\") > 0) {
 				String nodeName = path.substring(0, path.indexOf("\\"));
-				//System.out.println(nodeName);
+				// System.out.println(nodeName);
 				DefaultMutableTreeNode node1 = null;
-				for(int j=0; j<node.getChildCount();j++){
-					//System.out.println("CHILD:"+node.getChildAt(j));
-					if(((DefaultMutableTreeNode)node.getChildAt(j)).getUserObject().equals(nodeName)){
-						//System.out.println("FOUND"+nodeName);
-						node1 = (DefaultMutableTreeNode)node.getChildAt(j);
+				for (int j = 0; j < node.getChildCount(); j++) {
+					// System.out.println("CHILD:"+node.getChildAt(j));
+					if (((DefaultMutableTreeNode) node.getChildAt(j)).getUserObject().equals(nodeName)) {
+						// System.out.println("FOUND"+nodeName);
+						node1 = (DefaultMutableTreeNode) node.getChildAt(j);
 						break;
 					}
 				}
-				if(node1==null) node1 = new DefaultMutableTreeNode(nodeName);
+				if (node1 == null)
+					node1 = new DefaultMutableTreeNode(nodeName);
 				node.add(node1);
 				node = node1;
-				path = path.substring(path.indexOf("\\")+1);
+				path = path.substring(path.indexOf("\\") + 1);
 			}
-			//System.out.println(path);
-			node.add(new DefaultMutableTreeNode(header+path));
-			//System.out.println(path.substring(path.indexOf("\\")+1));
-			//root.add(new DefaultMutableTreeNode(header+v.get(i).name));
+			// System.out.println(path);
+			node.add(new DefaultMutableTreeNode(header + path));
+			// System.out.println(path.substring(path.indexOf("\\")+1));
+			// root.add(new DefaultMutableTreeNode(header+v.get(i).name));
 		}
 	}
-	
-	void showStatus(){
+
+	void showStatus() {
 		ArrayList<Video> v = db.getVideo();
-		int count=0;
+		int count = 0;
 		for (int i = 0; i < v.size(); i++) {
-			if(v.get(i).watch==1) count++;
+			if (v.get(i).watch == 1)
+				count++;
 		}
-		jl.setText(""+v.size()+"本中"+count+"本見ました！");
-		
+		jl.setText("" + v.size() + "本中" + count + "本見ました！");
+
 	}
 
 	public void valueChanged(TreeSelectionEvent e) {
 		try {
 			TreePath path = tree.getSelectionPath();
 			DefaultMutableTreeNode node = (DefaultMutableTreeNode) path.getLastPathComponent();
-			String file = node.toString().substring(3);
-			//System.out.println(file);
-			// Runtime r = Runtime.getRuntime();
+			DefaultMutableTreeNode node0 = node;
+			String headerfile = node.toString();
+			String file = headerfile.substring(headerfile.indexOf("]") + 1);
+			// System.out.println(file);
+			while (node.getParent() != null) {
+				file = node.getParent().toString() + "\\" + file;
+				node = (DefaultMutableTreeNode) node.getParent();
+			}
+			file = ".\\" + file;
+			System.out.println(file);
 
-			// Process process = r.exec("C:\\Program Files
-			// (x86)\\VideoLAN\\VLC\\vlc.exe "+node);
-			
-			//file="FIGHTERS2";
 			ProcessBuilder pb = new ProcessBuilder("C:\\Program Files (x86)\\VideoLAN\\VLC\\vlc.exe", file);
 			Process process = pb.start();
-			int ret = process.waitFor();
-			
-			node.setUserObject(new DefaultMutableTreeNode("[済]"+file));
-			db.setVideo(file,1);
-			showStatus();
+			process.waitFor();
+
+			checkVideo(node0);
+
+			//model.reload();
+			// node.setUserObject(new DefaultMutableTreeNode("[済]"+file));
+			// db.setVideo(file,1);
+			// showStatus();
 			tree.clearSelection();
 		} catch (Exception ex) {
-			//ex.printStackTrace();
+			// ex.printStackTrace();
 		}
-		
+	}
 
+	void checkVideo(DefaultMutableTreeNode node) {
+		System.out.println(node.toString() + node.getChildCount());
+		if (node.getChildCount() == 0) {
+
+			String file = "[済]" + node.toString().substring(3);
+			System.out.println(node.toString()+":"+file);
+			node.setUserObject(new DefaultMutableTreeNode(file));
+			model.nodeChanged(node);
+			return;
+		}
+
+		for (int i = 0; i < node.getChildCount(); i++) {
+			DefaultMutableTreeNode child = (DefaultMutableTreeNode) node.getChildAt(i);
+			checkVideo(child);
+		}
+		return;
 	}
 
 }
